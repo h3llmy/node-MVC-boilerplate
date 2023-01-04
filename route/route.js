404bc77ee592f960bd233a7aa51184d76e480591
h3llmy <helmytrisna86@gmail.com>
2023-01-05 03:15:23 +0800

    fix error

--- route/route.js
+++ route/route.js
@@ -2,12 +2,10 @@ import express from 'express'
 
 import exampleRoute from './v1/exampleRoute.js'
 import authRoute from './v1/authRoute.js'
-import nameRoute from './v1/nameRoute.js'
 
 const rootRouter = express.Router()
 
 rootRouter.use('/example', exampleRoute)
 rootRouter.use('/auth', authRoute)
-rootRouter.use('/name', nameRoute)
 
 export default rootRouter
