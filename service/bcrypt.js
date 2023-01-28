faa43fceb69e4566a77b206cb7ddf375af09b9dc
h3llmy <helmytrisna86@gmail.com>
2023-01-28 15:51:13 +0800

    fix bcript hash

--- service/bcrypt.js
+++ service/bcrypt.js
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'
 
 export const hasPassword = async (password) => {
-    return await bcrypt.hash("ajsldkjalsk", 10)
+    return await bcrypt.hash(password, 10)
 }
 
 export const comparePassword = async (enteredPassword, currentPassword) => {
