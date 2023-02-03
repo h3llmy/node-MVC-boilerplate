0783583c523f61897e3a758ea52dfca6a4577c57
h3llmy <helmytrisna86@gmail.com>
2023-02-03 09:11:01 +0800

    add middleware cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import cors from 'cors'
 import connectMongoDB from './connection/mongoDB.js'
 import router from './route/route.js'
 import fileUpload from 'express-fileupload'
 import helmet from 'helmet'
+import corsMiddleware from './middleware/corsMiddleware.js'
 
 dotenv.config()
 
@@ -23,25 +23,7 @@ app.use(fileUpload(), (req, res, next) => {
 })
 app.use(express.json())
 app.use(express.static('public'))
-var appOrigin = process.env.CORS_ORIGIN
-if (appOrigin) {
-  appOrigin = appOrigin.split(', ')
-}
-if (!appOrigin || appOrigin == '' || appOrigin.length <= 0) {
-  console.log('\x1b[34m%s\x1b[0m', 'all origin is allowed');
-} else {
-  console.log('\x1b[34m%s\x1b[0m', 'allowed origin : ', appOrigin);
-}
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!appOrigin || appOrigin == "[]") return callback(null, true);
-    if (appOrigin.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from this Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(corsMiddleware)
 
 app.use('/api/v1', router)
 
---
+++ middleware/corsMiddleware.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+import cors from 'cors'
+
+dotenv.config()
+
+var appOrigin = process.env.CORS_ORIGIN
+if (appOrigin) {
+    appOrigin = appOrigin.replace(' ', '').split(',')
+}
+if (!appOrigin || appOrigin == '' || appOrigin.length <= 0) {
+    console.log('\x1b[34m%s\x1b[0m', 'all origin is allowed');
+} else {
+    console.log('\x1b[34m%s\x1b[0m', 'allowed origin : ', appOrigin);
+}
+
+export default cors({
+    origin: (origin, callback) => {
+        if (!appOrigin || appOrigin == "[]") return callback(null, true);
+        if (appOrigin.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from this Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+});
