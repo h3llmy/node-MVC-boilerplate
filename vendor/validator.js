c02ac456f12cc057fdc5af798e3d53788dd26d0e
h3llmy <helmytrisna86@gmail.com>
2023-03-19 13:07:43 +0800

    fix validator

--- vendor/validator.js
+++ vendor/validator.js
@@ -23,70 +23,107 @@ export const urlCheck = (url) => {
     )
 }
 
-export default function validate(object, rules) {
+function validateObject(object, rules, prefix = '') {
   let errors = {}
+  let validated = {}
   for (const key in rules) {
     if (rules.hasOwnProperty(key)) {
       const rule = rules[key]
       let value = object[key]
+      let fieldPrefix = prefix ? `${prefix}.${key}` : key
 
       if (rule.required && !value) {
-        errors[key] = `${key} is required`
+        errors[fieldPrefix] = `${fieldPrefix} is required`
       } else {
         if (rule.type == Number) {
           value = Number(value)
           if (!value) {
-            errors[key] = `${key} should be of type ${rule.type.name}`
+            errors[fieldPrefix] = `${fieldPrefix} should be of type ${rule.type.name}`
           }
         } else if (value && rule.type && value.constructor !== rule.type) {
-          errors[key] = `${key} should be of type ${rule.type.name}`
+          errors[fieldPrefix] = `${fieldPrefix} should be of type ${rule.type.name}`
         }
         if (rule.min) {
           if (typeof value === 'number') {
             if (value < rule.min) {
-              errors[key] = `${key} minimum length of ${rule.min}`
+              errors[fieldPrefix] = `${fieldPrefix} minimum length of ${rule.min}`
             }
           }
           else if (value.length < rule.min) {
-            errors[key] = `${key} minimum length of ${rule.min}`
+            errors[fieldPrefix] = `${fieldPrefix} minimum length of ${rule.min}`
           }
         }
 
         if (rule.max) {
           if (typeof value === 'number') {
             if (value > rule.max) {
-              errors[key] = `${key} maximum length of ${rule.max}`
+              errors[fieldPrefix] = `${fieldPrefix} maximum length of ${rule.max}`
             }
           } else if (value.length > rule.max) {
-            errors[key] = `${key} maximum length of ${rule.max}`
+            errors[fieldPrefix] = `${fieldPrefix} maximum length of ${rule.max}`
           }
         }
-
+        if (value && typeof value === 'object' && !Array.isArray(value)) {
+          const nestedValidation = validateObject(value, rule.fields, fieldPrefix)
+          errors = { ...errors, ...nestedValidation.errors }
+          validated[key] = nestedValidation.validated
+        } else if (value && Array.isArray(value)) {
+          if (rule.fields && typeof rule.fields === 'object') {
+            validated[key] = []
+            value.forEach((item, index) => {
+              const nestedValidation = validateObject(item, rule.fields, `${fieldPrefix}[${index}]`)
+              errors = { ...errors, ...nestedValidation.errors }
+              validated[key].push(nestedValidation.validated)
+            })
+          } else {
+            errors[fieldPrefix] = `${fieldPrefix} should be of type array of objects`
+          }
+        }
         if (rule.isEmail) {
           if (!emailCheck(value)) {
-            errors[key] = `${key} should be email`
+            errors[fieldPrefix] = `${fieldPrefix} should be email`
           }
         }
 
         if (rule.isPhone) {
           if (!phoneCheck(value)) {
-            errors[key] = `${key} should be phone number`
+            errors[fieldPrefix] = `${fieldPrefix} should be phone number`
           }
         }
 
         if (rule.isUrl) {
           if (!urlCheck(value)) {
-            errors[key] = `${key} should be url`
+            errors[fieldPrefix] = `${fieldPrefix} should be url`
           }
+        }
+
+        if (rule.pattern) {
+          const regex = new RegExp(rule.pattern)
+          if (!regex.test(value)) {
+            errors[fieldPrefix] = `${fieldPrefix} does not match the required pattern`
+          }
+        }
+
+        if (value) {
+          validated[key] = value
         }
       }
     }
   }
+  return { errors, validated }
+}
+
+
+
+export default function validate(object, rules) {
+  const { errors, validated } = validateObject(object, rules)
   if (Object.keys(errors).length > 0) {
     let errorMessage = {}
     errorMessage.message = `error validations`
     errorMessage.path = errors
     errorMessage.statusCode = 400
     throw errorMessage
+  } else {
+    return validated
   }
 }
